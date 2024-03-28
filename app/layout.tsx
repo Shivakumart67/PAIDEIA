f8e2232d147f191dadf536476dc0295ab6ff7d9d
shivakumart67 <shivakumart67@gmail.com>
2024-03-28 21:15:32 +0530

    testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
-import type { Metadata } from "next";
+import { ClerkProvider } from '@clerk/nextjs'
+import './globals.css'
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "PAIDEIA",
-  description: "LMS PLATFORM TO EXPLORE WHAT EVER YOU WANT",
-};
+  title: 'PAIDEIA',
+  description: 'LMS PLATFORM TO EXPLORE WHAT EVER YOU WANT...',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang='en'>
         <body>{children}</body>
       </html>
     </ClerkProvider>
-  );
+  )
 }
