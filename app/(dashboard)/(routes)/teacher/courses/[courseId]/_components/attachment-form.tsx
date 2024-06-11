8f6f333941a57e0490517c7dfe3924f65f825ab3
Shivakumara T <shivakumart67@gmail.com>
2024-06-11 15:34:45 +0000

    added new schema and api

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -4,22 +4,20 @@ import * as z from 'zod'
 import axios from 'axios'
 import { useState } from 'react'
 import { Button } from '@/components/ui/button'
-import { ImageIcon, Pencil, PlusCircle } from 'lucide-react'
+import { File, ImageIcon, Loader2, Pencil, PlusCircle, X } from 'lucide-react'
 import toast from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
-import { Course } from '@prisma/client'
+import { Attachment, Course } from '@prisma/client'
 import Image from 'next/image'
 import { FileUpload } from '@/components/file-upload'
 
 interface AttachmentFormProps {
-  initialData: Course
+  initialData: Course & { attachments: Attachment[] }
   courseId: string
 }
 
 const formSchema = z.object({
-  imageUrl: z.string().min(1, {
-    message: 'Image is required',
-  }),
+  url: z.string().min(1),
 })
 
 export const AttachmentForm = ({
@@ -27,6 +25,9 @@ export const AttachmentForm = ({
   courseId,
 }: AttachmentFormProps) => {
   const [isEditing, setIsEditing] = useState(false)
+
+  const [deletingId, setDeletingId] = useState<string | null>(null)
+
   const router = useRouter()
   const toggleEdit = () => {
     setIsEditing(!isEditing)
@@ -34,65 +35,89 @@ export const AttachmentForm = ({
 
   const onSubmit = async (value: z.infer<typeof formSchema>) => {
     try {
-      console.log(value)
-      await axios.patch(`/api/courses/${courseId}`, value)
-      toast.success('Image updated Successfully')
+      await axios.post(`/api/courses/${courseId}/attachments`, value)
+      toast.success('Attachment added updated Successfully')
       toggleEdit()
       router.refresh()
     } catch (error) {
       toast.error('Something wents wrong')
     }
   }
+  const onDelete = async (id: string) => {
+    try {
+      setDeletingId(id)
+      await axios.delete(`/api/courses/${courseId}/attachments/${id}`)
+      toast.success('Attachment Deleted')
+      router.refresh()
+    } catch (error) {
+      toast.error('Someting wents wrong')
+    } finally {
+      setDeletingId(null)
+    }
+  }
 
   return (
     <div className='mt-6 border bg-slate-100 rounded-md p-4'>
       <div className='font-medium flex items-center justify-between'>
-        Course Image
+        Course Attachments
         <Button variant={'ghost'} onClick={toggleEdit}>
           {isEditing && <>Cancel</>}
 
-          {!isEditing && initialData?.imageUrl && (
-            <>
-              <Pencil className='h-4 w-4 mr-2' />
-              Edit Image
-            </>
-          )}
-
-          {!isEditing && !initialData?.imageUrl && (
+          {!isEditing && (
             <>
               <PlusCircle className='h-4 w-4 mr-2' />
-              Add Image
+              Add Attachments
             </>
           )}
         </Button>
       </div>
       {!isEditing ? (
-        !initialData?.imageUrl ? (
-          <div className='flex items-center justify-center h-60 bg-slate-200 rounded-md'>
-            <ImageIcon className='h-10 w-10 text-slate-500' />
-          </div>
-        ) : (
-          <div className='relative aspect-video mt-2'>
-            <Image
-              alt='Upload'
-              fill
-              className='object-cover rounded-md'
-              src={initialData?.imageUrl || ''}
-            />
-          </div>
-        )
+        <>
+          {initialData.attachments.length === 0 && (
+            <p className='text-sm mt-2 text-slate-500 italic'>
+              No Attachments yet
+            </p>
+          )}
+          {initialData.attachments.length > 0 && (
+            <div className='space-y-2'>
+              {initialData.attachments.map((attachment) => (
+                <div
+                  key={attachment.id}
+                  className='flex items-center p-3 w-full bg-sky-100 border-sky-200 text-sky-700 rounded-md'
+                >
+                  <File className='h-4 w-4 mr-2 flex-shrink-0' />
+                  <p className='text-xs line-clamp-1'>{attachment.name}</p>
+                  {deletingId === attachment.id && (
+                    <div className='ml-auto'>
+                      <Loader2 className='h-4 w-4 animate-spin' />
+                    </div>
+                  )}
+                  {deletingId !== attachment.id && (
+                    <button
+                      onClick={() => onDelete(attachment.id)}
+                      className='ml-auto hover:opacity-75 transition'
+                    >
+                      <X className='h-4 w-4' />
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
+        </>
       ) : (
         <div>
           <FileUpload
-            endpoint='courseImage'
+            endpoint='courseAttachment'
             onChange={(url) => {
               if (url) {
-                onSubmit({ imageUrl: url })
+                onSubmit({ url: url })
               }
             }}
           />
           <div className='text-xs text-muted-foreground mt-4'>
-            16:9 Aspect ratio recomended
+            Add anything to your students that is good to have about this
+            course.
           </div>
         </div>
       )}
